8cfe96a1ca9d26f76d0a81594d00746dcee7277e
koki hatada <koki.hatada@gmail.com>
2018-09-29 19:43:23 +0900

    コマンドから起動

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 "use strict"
 
-import * as path from "path";
+import * as url from "url";
 import * as http from "http";
 import WebSocket = require("ws");
+import * as querystring from "querystring";
 const HttpsProxyAgent = require("https-proxy-agent");
-import * as request from "request";
-
-const channel = "test";
-const base = "http://localhost:3000/";
+import * as commander from "commander";
+const packageJson = require("../package.json");
 
 export type Method = "GET" | "POST" | "PUT" | "DELETE";
 
@@ -39,7 +38,7 @@ function uniqueId(n: number = 8): string {
 
 function ignoreHeader(header: string): boolean {
     const lower = header.toLowerCase();
-    const ignore = ["connection", "host", "content-length"];
+    const ignore = ["connection", "host"];
     return ignore.indexOf(lower) >= 0;
 }
 
@@ -50,6 +49,7 @@ export class TunnelClient {
     readonly proxy: string;
     readonly heartBeatInterval: number;
     readonly target: string;
+    readonly authToken: string;
 
     private ws: WebSocket;
     private needsReconnect: boolean = true;
@@ -58,16 +58,17 @@ export class TunnelClient {
     private queue: Buffer[] = [];
     private isSending: boolean = false;
 
-    private request: {[id: string]: request.Request} = {};
+    private request: {[id: string]: http.ClientRequest} = {};
 
     static readonly MESSAGE_ID_HEADER = 8;
     static readonly MESSAGE_COMMAND_HEADER = 1;
 
-    constructor(options: { channel: string, host: string, proxy: string, target: string, heartBeatInterval?: number }) {
+    constructor(options: { channel: string, host: string, proxy: string, target: string, heartBeatInterval?: number, authToken: string }) {
         this.host = options.host;
         this.target = options.target;
         this.proxy = options.proxy;
         this.channel = options.channel;
+        this.authToken = options.authToken;
         if (options.heartBeatInterval && options.heartBeatInterval > 0) {
             this.heartBeatInterval = options.heartBeatInterval;
         } else {
@@ -94,7 +95,7 @@ export class TunnelClient {
         if (this.proxy) {
             agent = new HttpsProxyAgent(this.proxy);
         }
-        const serverUrl = this.host + "/hoop/" + this.channel;
+        const serverUrl = this.host + "/hoop/" + this.channel + "?" + querystring.stringify({auth: this.authToken});
         console.log(new Date(), `Connect to server. url=${serverUrl}`);
         this.ws = new WebSocket(serverUrl, { agent: agent });
         this.ws.on("open", this.onOpen);
@@ -131,7 +132,7 @@ export class TunnelClient {
             const commandBuffer = data.slice(TunnelClient.MESSAGE_ID_HEADER, TunnelClient.MESSAGE_ID_HEADER + TunnelClient.MESSAGE_COMMAND_HEADER);
             const id = idBuffer.toString("utf-8", 0, TunnelClient.MESSAGE_ID_HEADER);
             const command = commandBuffer.toString();
-            // console.log(`Tunnel message. id=${id} command=${command}`);
+            console.log(`Tunnel message. id=${id} command=${command}`);
             if (command === "h") {
                 const headerBuffer = data.slice(TunnelClient.MESSAGE_ID_HEADER + TunnelClient.MESSAGE_COMMAND_HEADER);
                 const header = JSON.parse(headerBuffer.toString());
@@ -141,32 +142,49 @@ export class TunnelClient {
                         headers[name] = header.headers[name];
                     }
                 });
-                const options: request.CoreOptions = {
+                console.log(header);
+                const targetRawUrl = this.target + header.path + (header.query ? `?${header.query}` : "");
+                const targetUrl = url.parse(targetRawUrl);
+                const options: http.RequestOptions = {
                     method: header.method,
                     headers,
-                    proxy: null,
-                    timeout: 30000
+                    timeout: 30000,
+                    protocol: targetUrl.protocol,
+                    hostname: targetUrl.hostname,
+                    port: targetUrl.port,
+                    path: targetUrl.path
                 };
-                const targetURL = this.target + header.path + (header.query ? `?${header.query}` : "");
-                this.request[id] = request(targetURL, options);
-                this.request[id].on("response", (res) => {
+
+                this.request[id] = http.request(options);
+                this.request[id].on("response", (res: http.IncomingMessage) => {
+                    res.on("data", (data: Buffer) => {
+                        console.log(`Request command=s`, data.toString());
+                        const commandBuffer = new Buffer("s");
+                        const message = Buffer.concat([idBuffer, commandBuffer, data]);
+                        this.sendToServer(message);
+                    });
+                    res.on("end", () => {
+                        console.log(`Request command=e`);
+                        const commandBuffer = new Buffer("e");
+                        const message = Buffer.concat([idBuffer, commandBuffer]);
+                        this.sendToServer(message);
+                        delete this.request[id];
+                    });
+                    res.on("error", (error) => {
+                        console.log(`Request command=a`, error);
+                        const commandBuffer = new Buffer("a");
+                        const message = Buffer.concat([idBuffer, commandBuffer]);
+                        this.sendToServer(message);
+                        delete this.request[id];
+                    });
+                    console.log(`Request command=h`);
                     const commandBuffer = new Buffer("h");
                     const header = JSON.stringify({headers: res.headers, status: res.statusCode});
                     const message = Buffer.concat([idBuffer, commandBuffer, new Buffer(header)]);
                     this.sendToServer(message);
                 });
-                this.request[id].on("data", (data: Buffer) => {
-                    const commandBuffer = new Buffer("s");
-                    const message = Buffer.concat([idBuffer, commandBuffer, data]);
-                    this.sendToServer(message);
-                });
-                this.request[id].on("end", () => {
-                    const commandBuffer = new Buffer("e");
-                    const message = Buffer.concat([idBuffer, commandBuffer]);
-                    this.sendToServer(message);
-                    delete this.request[id];
-                });
-                this.request[id].on("error", () => {
+                this.request[id].on("error", (error) => {
+                    console.log(`Request command=a`, error);
                     const commandBuffer = new Buffer("a");
                     const message = Buffer.concat([idBuffer, commandBuffer]);
                     this.sendToServer(message);
@@ -175,6 +193,7 @@ export class TunnelClient {
             } else if (command === "s") {
                 const dataBuffer = data.slice(TunnelClient.MESSAGE_ID_HEADER + TunnelClient.MESSAGE_COMMAND_HEADER);
                 if(this.request[id]) {
+                    console.log(dataBuffer.toString());
                     this.request[id].write(dataBuffer);
                 }
             } else if (command === "e") {
@@ -193,7 +212,7 @@ export class TunnelClient {
     }
 
     private sendHeartBeat(): void {
-        console.log(new Date(), `Send heart beat. channel=${channel}`);
+        console.log(new Date(), `Send heart beat. channel=${this.channel}`);
         if (this.ws) {
             const id = uniqueId(TunnelClient.MESSAGE_ID_HEADER);
             this.sendToServer(new Buffer(id));
@@ -225,4 +244,26 @@ function getProxyFromEnvironment(): string {
     return process.env.https_proxy || process.env.HTTPS_PROXY;
 }
 const proxy = getProxyFromEnvironment();
-const tunnel = new TunnelClient({channel: "test", host: "ws://localhost:8080", proxy: null, target: "http://localhost:4567"});
+
+const program = commander.version(packageJson.version)
+    .option("-c, --channel [value]", "channel")
+    .option("-t, --target [value]", "target host to which request are piped")
+    .option("-s, --server [value]", "hoop server url (starts with ws[s]://)")
+    .option("-a, --auth [value]", "authentication token for the server")
+    .parse(process.argv)
+const options = program.opts();
+if (options.help) {
+    program.help();
+} else {
+    const tunnelClientOptions = {
+        channel: options.channel || "test",
+        host: options.server || "ws:localhost:80",
+        proxy: proxy,
+        target: options.target || "http://localhost:8080",
+        authToken: options.auth || ""
+    }
+    Object.keys(tunnelClientOptions).forEach(key => {
+        console.log(`${key}=${tunnelClientOptions[key]}`);
+    })
+    new TunnelClient(tunnelClientOptions);
+}
